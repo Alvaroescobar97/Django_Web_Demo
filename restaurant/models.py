e53e869f79bf5b886605caeb975cbcce9828685b
Alvaroescobar97 <alvaro325@hotmail.com>
2021-03-14 13:00:01 -0500

    Final details

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
 # Create your models here.
+ 
+class Client(models.Model):
+    name = models.CharField(max_length=50)
+    phone = models.IntegerField()
+    email =  models.EmailField(max_length = 50) 
+ 
+    def __str__(self):
+        return f"id={self.id},name={self.name},phone={self.phone},email={self.email}"
+    
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.FloatField() 
+    description = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return f"id={self.id},name={self.name},price={self.price},description={self.description}"    
+    
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    product = models.ManyToManyField(Product)
+    
+    def __str__(self):
+        return f"id={self.id}, client={self.client}"
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Order
+ 
 # Create your views here.
+def index(request):
+    orders = Order.objects.all()
+    context = {
+        'titulo_pagina': 'Ordenes del Restaurante',
+        'listado_ordenes': orders
+    }
+    return render(request, 'orders.html' ,context)
